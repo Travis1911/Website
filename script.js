c59f1f71595d210109419de5e38bab1c7630bbd7
Travis1911 <Jeeth_1930@hotmail.com>
2026-03-24 15:59:41 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿function moveBox() {
+    let box = document.getElementById("box");
+    let pos = 0;
+
+    let interval = setInterval(() => {
+        if (pos >= 300) {
+            clearInterval(interval);
+        } else {
+            pos++;
+            box.style.left = pos + "px";
+        }
+    }, 5);
+} 
